b76a659b3c0a34d3dec63f1ccf0b70b59a07cccb
tarequnes <unestarq@gmail.com>
2026-06-10 17:06:48 +0100

    version 2

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
+app.get("/api/status", (req, res) => {
     res.json({
         message: "Application GreenFood fonctionne"
     });
